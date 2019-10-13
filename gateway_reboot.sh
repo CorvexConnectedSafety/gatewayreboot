c65d7686373dfebb262c0ea4aa11a697af24e444
mrjonestech <misterjonestech@gmail.com>
2019-10-13 16:29:59 -0500

    initial upload of the reboot script and the ssh config file

---
+++ gateway_reboot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script will take a single commandline argument: the hostname of the target gateway to be rebooted.
+# We will compare the argument against the contents of the user's .ssh/config file to determine if the gateway is listed.
+# If the gateway is listed in .ssh/config, the we will continue with the reboot
+# If it is not, we will notify the user that it needs to be configured before continuing and will exit the script at that point.
+# 
+# Ideally all gateways should be converted to passwordless ssh access similar to twb-an's current configuration.
+# 
+# /home/<username>/.ssh/config will need to be created for each user with each gateway's hostname.
+#
+# an example config file "config" is included with this script in it's repo. 
+# Edit this file to reflect the current username and point to the proper keyfile with IdentityFile.
+
+gateway="$1"
+username=$(whoami)
+echo $gateway
+echo $username
+
+if ! grep -Fxq "$gateway" /home/"$username"/.ssh/config ;
+	then 
+		echo "This gateway does not exist in .ssh/config. Please add an entry for "$gateway" to the file and re-run."
+		exit 1
+fi
+
+echo "Reboot gateway "$gateway"? (y/n)"
+
+
